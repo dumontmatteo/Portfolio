25a027e9401d01b53b1ef4ab4ae44a994d492a34
Matteo Dumont <matteo.dumont@insa-lyon.fr>
2021-02-18 22:00:47 +0100

    image performace boost and languange change

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 var img = document.getElementById("img");
 
+
 function setImage(){
 	var source = event.srcElement.id;
 	console.log(source);
 	switch(source){
 		case 'adesit':
-			img.src = 'images/adesit.jpg';
+			img.src = 'images/adesit-min.png';
 			break;
 		case 'tedxinsa':
-			img.src = 'images/tedxinsa.jpg';
+			img.src = 'images/tedxinsa-min.png';
 			break;
 		case 'zaybe':
-			img.src = 'images/zaybe.png';
+			img.src = 'images/zaybe-min.png';
 			break;
 		default:
 			console.log("none");
@@ -22,4 +23,34 @@ function setImage(){
 function resetImage(){
 	img.src = '';
 
+}
+
+function toFrench(argument) {
+	document.getElementById("title").innerHTML = "Eleve-Ingénieur en Informatique à l'INSA de Lyon, à la recherche d'un stage de 3 mois en developpement front-end.";
+	document.getElementById("sub-title").innerHTML = "Passioné de design je cherche aussi à consolider et developper mes connaissances en developpement.";
+	document.getElementById("button").innerHTML = "VOIR MON CV";
+	document.getElementById("work-title").innerHTML = "PROJETS";
+	document.getElementById("footer-title").innerHTML = "Mes réseaux";
+	document.getElementById("cv").href = "files/cv_fr.pdf";
+	
+	if (document.documentElement.lang == "en") {
+		document.getElementById("underline").style.transform = "translateX(-35px)";
+	}
+
+	document.documentElement.lang = "fr"; 
+}
+
+function toEnglish(argument) {
+	document.getElementById("title").innerHTML = "I'm an IT Engineering student at INSA Lyon looking for a 3-month front-end web development internship.";
+	document.getElementById("sub-title").innerHTML = "I am passionate about design and am looking forward to enhance my development skills.";
+	document.getElementById("button").innerHTML = "SEE MY RESUME";
+	document.getElementById("work-title").innerHTML = "MY WORK";
+	document.getElementById("footer-title").innerHTML = "My Socials";
+	document.getElementById("cv").href = "files/resume_en.pdf";
+
+	if (document.documentElement.lang == "fr") {
+		document.getElementById("underline").style.transform = "translateX(0px)";
+	}
+
+	document.documentElement.lang = "en";
 }
