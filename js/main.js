548a57b09cf81c26424f72776a00840da7fe15a5
Matteo Dumont <matteo.dumont@insa-lyon.fr>
2021-02-19 10:05:21 +0100

    button fix and pdf change

--- js/main.js
+++ js/main.js
@@ -26,12 +26,12 @@ function resetImage(){
 }
 
 function toFrench(argument) {
-	document.getElementById("title").innerHTML = "Eleve-Ingénieur en Informatique à l'INSA de Lyon, à la recherche d'un stage de 3 mois en developpement front-end.";
+	document.getElementById("title").innerHTML = "Eleve-Ingénieur en Informatique à l'INSA de Lyon, à la recherche d'un stage de 3 mois en developpement web front-end.";
 	document.getElementById("sub-title").innerHTML = "Passioné de design je cherche aussi à consolider et developper mes connaissances en developpement.";
 	document.getElementById("button").innerHTML = "VOIR MON CV";
-	document.getElementById("work-title").innerHTML = "PROJETS";
+	document.getElementById("work-title").innerHTML = "VOIR MES PROJETS";
 	document.getElementById("footer-title").innerHTML = "Mes réseaux";
-	document.getElementById("cv").href = "files/cv_fr.pdf";
+	document.getElementById("cv").href = "files/cv_dumont_matteo.pdf";
 	
 	if (document.documentElement.lang == "en") {
 		document.getElementById("underline").style.transform = "translateX(-35px)";
@@ -44,9 +44,9 @@ function toEnglish(argument) {
 	document.getElementById("title").innerHTML = "I'm an IT Engineering student at INSA Lyon looking for a 3-month front-end web development internship.";
 	document.getElementById("sub-title").innerHTML = "I am passionate about design and am looking forward to enhance my development skills.";
 	document.getElementById("button").innerHTML = "SEE MY RESUME";
-	document.getElementById("work-title").innerHTML = "MY WORK";
+	document.getElementById("work-title").innerHTML = "SEE MY WORK";
 	document.getElementById("footer-title").innerHTML = "My Socials";
-	document.getElementById("cv").href = "files/resume_en.pdf";
+	document.getElementById("cv").href = "files/resume_dumont_matteo.pdf";
 
 	if (document.documentElement.lang == "fr") {
 		document.getElementById("underline").style.transform = "translateX(0px)";
